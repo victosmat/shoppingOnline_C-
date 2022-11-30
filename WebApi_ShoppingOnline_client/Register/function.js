897cda9b2e115c8b24ee4de47658a08f551b030a
nghia <nguyenminhnghiadev@gmail.com>
2022-11-30 22:56:10 +0700

    update client

--- WebApi_ShoppingOnline_client/Register/function.js
+++
@@ -1,6 +0,0 @@
-let username = document.getElementById("username");
-let email = document.getElementById("email");
-let password = document.getElementById("password");
-let btnSignup = document.querySelector(".btn-signup");
-let btnLogin = document.querySelector(".btn-login");
-
